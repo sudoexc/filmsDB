2e7a695521a1210bdce33b3531f8f58286806a20
Joe <javokhir.369@gmail.com>
2024-02-25 23:37:26 +0500

    25 feb

--- js/script.js
+++ js/script.js
@@ -44,46 +44,113 @@ P.S. Здесь есть несколько вариантов решения з
 
 
 
+// 1) Реализовать функционал, что после заполнения формы и нажатия кнопки "Подтвердить" - 
+// новый фильм добавляется в список. Страница не должна перезагружаться.
+// Новый фильм должен добавляться в movieDB.movies.
+// Для получения доступа к значению input - обращаемся к нему как input.value;
+// P.S. Здесь есть несколько вариантов решения задачи, принимается любой, но рабочий.
+
+// 2) Если название фильма больше, чем 21 символ - обрезать его и добавить три точки
+
+// 3) При клике на мусорную корзину - элемент будет удаляться из списка (сложно)
+
+// 4) Если в форме стоит галочка "Сделать любимым" - в консоль вывести сообщение: 
+// "Добавляем любимый фильм"
+
+// 5) Фильмы должны быть отсортированы по алфавиту */
+
+
+
+
 'use strict';
 
+document.addEventListener('DOMContentLoaded', () => {
+    
+    const movieDB = {
 
+        movies: [   
+            'Логан', 
+            'Лига справедливости',
+            'Ла-ла лэнд',
+            'Одержимость', 
+            'Скотт Пилигрим против...'
+        ]
+    
+    };
+    
+    
+    const adds = document.querySelectorAll('.promo__adv img'),
+          bg = document.querySelector('.promo__bg'),
+          genre = bg.querySelector('.promo__genre'),
+          filmsList = document.querySelector('.promo__interactive-list'),
+          addForm = document.querySelector('form.add'),
+          addInput = addForm.querySelector('.adding__input'),
+          checkbox = addForm.querySelector('[type="checkbox"]');
 
-const movieDB = {
+          addForm.addEventListener('submit', (event) => {
+            event.preventDefault();
 
-    movies: [
+            let newFilm = addInput.value;
+            const favorite = checkbox.checked;
 
-        'Логан',
+            if (newFilm) {
 
-        'Лига справедливости',
+                if (newFilm.length > 21) {
+                    newFilm = `${newFilm.substring(0, 22)}...`;
+                }
 
-        'Ла-ла лэнд',
+                if (favorite) {
+                    console.log('Добавляем любимый фильм');
+                }
 
-        'Одержимость',
+                movieDB.movies.push(newFilm);
+                sortArr(movieDB.movies);
+    
+                createMovieList(movieDB.movies, filmsList);
+            }
 
-        'Скотт Пилигрим против...'
+            event.target.reset();
+          });
+    
+    const deleteAdv = (arr) => {
+        arr.forEach(item => {
+            item.remove();
+        });
+    };
+    deleteAdv(adds);
+    
+    const makeChanges = () => {
+        genre.textContent = 'ДРАММА';
+        
+        bg.style.backgroundImage = 'url(img/bg.jpg)';
+    };
+    
+    makeChanges();
+    
+        const sortArr = (arr) => {
+        arr.sort();
+    };
+    
+    function createMovieList(films, parent) {
+    parent.innerHTML = '';
+    sortArr(films);
+    
+    films.forEach((films, number) => {
+        parent.innerHTML += `<li class="promo__interactive-item">${number + 1})${films}
+        <div class="delete"></div>
+        </li>`;
+    });
 
-    ]
+    document.querySelectorAll('.delete').forEach((btn, i) => {
+        btn.addEventListener('click', () => {
+            btn.parentElement.remove();
+            movieDB.movies.splice(i, 1);
 
-};
+            createMovieList(films, parent);
+        });
+    });
+    }
 
+    createMovieList(movieDB.movies, filmsList);
 
-const adds = document.querySelectorAll('.promo__adv img'),
-      bg = document.querySelector('.promo__bg'),
-      genre = bg.querySelector('.promo__genre'),
-      filmsList = document.querySelector('.promo__interactive-list');
-
-adds.forEach(item => {
-    item.remove();
-});
-
-genre.textContent = 'ДРАММА';
-
-bg.style.backgroundImage = 'url(img/bg.jpg)';
-
-filmsList.innerHTML = '';
-
-movieDB.movies.sort();
-
-movieDB.movies.forEach((films, number) => {
-    filmsList.innerHTML += `<li>${number + 1})${films}</li>`;
 });
